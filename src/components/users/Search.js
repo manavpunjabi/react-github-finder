b57ea70c260085c9ef91726d0fc2e53e8e9931ee
Manav Punjabi <manavpunjabi26@gmail.com>
2020-02-12 15:44:10 +0530

    added clear button and empty field alert

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,33 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
 export class Search extends Component {
   state = {
     text: ""
   };
+
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
+  };
+
   onSubmit = e => {
     e.preventDefault();
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter a username", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
   };
+
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -27,6 +44,11 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
